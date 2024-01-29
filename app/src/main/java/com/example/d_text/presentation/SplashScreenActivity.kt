234058c4a0d167a6e4c9e1470f427f28d076a939
Ftittawat <tittawatjaiou@gmail.com>
2024-01-29 10:45:23 +0700

    init project

---
+++ app/src/main/java/com/example/d_text/presentation/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.d_text.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.d_text.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
+    }
+}
